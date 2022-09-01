80169193bfad49e5beabbb76170270308f36cf66
janshen <1134978545@qq.com>
2022-09-01 13:51:23 +0800

    完成个人中心功能的开发

---
+++ .history/assets/js/user/user_pwd_20220901105201.js
@@ -0,0 +1,22 @@
+$(function() {
+    var form = layui.form
+
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        //value 是新密码框输入的值
+        samePwd: function(value) {
+            if (value === $('[name=oldPwd]').val()) {
+                return '新旧密码不能相同！'
+            }
+        },
+        //value 是确认新密码框输入的值
+        rePwd: function(value) {
+            if (value !== $('[name=newPwd]').val()) {
+                return '两次输入密码不一致！'
+            }
+        }
+
+    })
+})
